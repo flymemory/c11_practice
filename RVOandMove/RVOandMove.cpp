14e4808de21a52f79da484b57eaa3efaf90b807d
flymemory <171507@gmail.com>
2016-11-05 12:50:40 +0800

    Add ROV and std::move sample.

--- RVOandMove/RVOandMove.cpp
+++ RVOandMove/RVOandMove.cpp
@@ -1,8 +1,11 @@
 // RVOandMove.cpp : Defines the entry point for the console application.
-//
+// This part is test Return Value Optimized and std::move
+#include <iostream>
+#include <vector>
 
 int main()
 {
+
     return 0;
 }
 
